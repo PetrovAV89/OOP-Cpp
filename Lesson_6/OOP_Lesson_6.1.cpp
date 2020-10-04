79d0fe825f70e1f26a5b01fee9a569f672697eb2
andrey <a.n.d.r.e.j.k.a@mail.ru>
2020-10-04 13:18:45 +0500

    Задание 6.1

---
+++ Lesson_6/OOP_Lesson_6.1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	int i;
+	while (true) {
+		cout << "Enter a number: ";
+		cin >> i;
+		if (cin.good()) {
+			cin.ignore(10, '\n');
+			break;
+		}
+		cin.clear();
+		cout << "Error!" << endl;
+		cin.ignore(10, '\n');	
+	}
+	cout << "Number: " << i << endl;
+
+
+	return 0;	
+}
+
