d5f1630a35265702e3422d83f7847ff1ac1fc47c
andrey <a.n.d.r.e.j.k.a@mail.ru>
2020-10-09 15:57:11 +0500

    Задание 8.1

---
+++ Lesson_8/OOP_Lesson_8.1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double div_1 (int a, int b) {
+	double res = 0;
+	if (!b) 
+		throw "Division By Zero";
+	else 
+		res = (double)a / b;
+	return res;
+}
+
+int main() {
+	int x, y;
+	cout << "Enter the first value: ";
+	cin >> x;
+	cout << "Enter the second value: ";
+	cin >> y;
+	try {
+		cout << div_1(x, y);
+	}		
+	catch (const char* ex) {
+		cerr << "Error: " << ex << endl;
+	}		
+
+	return 0;   
+}
+
